5f813a9cabe5c5ecfdb5e809a224b6c257167953
MukherjeeSouvik <souvik.mukherjee167@gmail.com>
2019-09-17 15:36:05 +0530

    Maven Project for Jenkins

---
+++ src/main/java/com/study/jenkins/springjenkinssvc/SpringJenkinsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.study.jenkins.springjenkinssvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsSvcApplication.class, args);
+	}
+
+}
